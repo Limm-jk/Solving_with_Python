07120a2ccad25667df1f659b2a5b107939b33dad
Limm-jk <201602057@cs-cnu.org>
2020-04-08 12:05:09 +0900

    reSolve 1904(01 Tail)

--- 동적계획/1904.py
+++ 동적계획/1904.py
@@ -7,9 +7,10 @@ N = int(input())
 arr = [0 for i in range(N)]
 
 arr[0] = 1
-arr[1] = 2
+if N != 1:
+    arr[1] = 2
 
-for i in range(2,N):
-    arr[i] = (arr[i-1]+arr[i-2])%15746
+    for i in range(2,N):
+        arr[i] = (arr[i-1]+arr[i-2])%15746
 
 print(arr[N-1])
