d1a7fe9317664d59be978326ed6a7f795dad2553
Limm-jk <201602057@cs-cnu.org>
2020-04-14 02:09:48 +0900

    fail to 1260

--- 탐색/1260.py
+++ 탐색/1260.py
@@ -6,20 +6,23 @@
 #  * @desc [DFS BFS]
 #  */
 
-# 0 = 연결 안됨 1 = 연결됨 2 = 탐색함
 
 import queue
+import sys 
+
+sys.setrecursionlimit(10**8)# 재귀 늘려줌
 
 arr = []
 arr2 = []
-def adj(G, p1, p2):
+
+def adj(G, p1, p2):#인접한 경우 True를 리턴
     if G[p1][p2] == 1:
         return True
     else:
         return False
 
 def dfs(G,v):
-    arr.append(v)
+    arr.append(v)# 중복확인겸 결과 배열
     for i in range(1,len(G)):
         if (adj(G,v,i)) and (i not in arr):
             dfs(G,i)
@@ -27,23 +30,39 @@ def dfs(G,v):
 def bfs(G,v):
     qu = queue.Queue()
     qu.put(v)
-    while qu.empty:
+    arr2.append(v)# 중복확인겸 결과 배열
+    while not qu.empty():
         X = qu.get()
-        arr2.append(X)
         for i in range(1,len(G)):
-            if (adj(G,v,i)) and (i not in arr2):
+            if (adj(G,X,i)) and (i not in arr2):
                 qu.put(i)
+                arr2.append(i)# 중복확인겸 결과 배열
+
+
+
 
 N, M, V = map(int, input().split())
 
 graph = [[0] * (N + 1) for _ in range(N + 1)]
 
-for _ in range(M):
-    x, y = map(int, input().split())
-    graph[x][y] = 1
-    graph[y][x] = 1
+# for _ in range(M):
+#     x, y = map(int, input().split())
+#     graph[x][y] = 1
+#     graph[y][x] = 1
+
+for i in range(1000):
+    graph[i][i+1] = 1
+
 
 dfs(graph,V)
 bfs(graph,V)
-print(arr)
-print(arr2)
+
+
+# 출력
+for i in range(len(arr)-1):
+    print(arr[i], end = " ")
+print(arr[len(arr)-1])
+
+for i in range(len(arr2)-1):
+    print(arr2[i], end = " ")
+print(arr2[len(arr2)-1])
