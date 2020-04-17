462417a5c98689ded037549e60de88c3c8814183
Limm-jk <201602057@cs-cnu.org>
2020-04-17 16:18:32 +0900

    solve 2667_DFS

---
+++ 탐색/2667.py
@@ -0,0 +1,59 @@
+# /**
+#  * @author [Limm-jk]
+#  * @email [201602057@cs-cnu.org]
+#  * @create date 2020-04-15 23:54:08
+#  * @modify date 2020-04-15 23:54:08
+#  * @desc [단지번호 / 탐색]
+#  */
+
+import sys
+
+search_arr = []
+
+sys.setrecursionlimit(10**8)
+
+# def search_adj(v1,v2):
+#     arr = []
+#     if v1 == 0 & v2==0:
+#         arr = [[v1+1,v2],[v1,v2+1]]
+#     elif v1 == 0:
+#         arr = [[v1+1,v2],[v1,v2+1],[v1,v2-1]]
+#     elif v2 == 0:
+#         arr = [[v1+1,v2],[v1,v2+1],[v1-1,v2]]
+#     else:
+#         arr = 
+    
+#     return arr
+def DFS(G, v1, v2):
+    G[v1][v2] = count
+    global danji_count
+    danji_count += 1
+    n = len(G)
+    adj_arr = [[v1+1,v2],[v1,v2+1],[v1,v2-1],[v1-1,v2]]
+    for i in adj_arr:
+        if i[0] < 0 or i[0] >= n or i[1] < 0 or i[1] >=n:
+            continue
+        if G[i[0]][i[1]] == 1:
+            DFS(G,i[0], i[1])
+
+N = int(input())
+danji_arr = [0]*N
+ful_arr = []
+danji_count = 0
+count = 1
+for i in range(N):
+    danji_arr[i] = list(map(int, input()))
+
+for i in range(N):
+    for j in range(N):
+        if danji_arr[i][j] == 1:
+            count += 1
+            DFS(danji_arr, i, j)
+            ful_arr.append(danji_count)
+            danji_count = 0
+
+ful_arr.sort()
+
+print(len(ful_arr))
+for i in ful_arr:
+    print(i)
