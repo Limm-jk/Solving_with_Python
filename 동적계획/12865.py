49cf3e58b5775960b196d477c101759b799c38b3
Limm-jk <201602057@cs-cnu.org>
2020-07-20 22:20:19 +0900

    [BOJ] 12865 - 배낭문제

--- 동적계획/12865.py
+++ 동적계획/12865.py
@@ -8,5 +8,21 @@
 
 N,K = map(int,input().split())  # 물품수와 최대무게
 
+table = [[0 for _ in range(K+1)] for _ in range(N+1)] # table[물품수][최대무게]
+products = [[0,0]]
+
 for i in range(N):
-    W,V = map(int,input().split())
+    products.append(list(map(int,input().split()))) # [무게,가치]
+
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        weight = products[i][0]
+        value = products[i][1]
+
+        if j >= weight:
+            table[i][j] = max(value + table[i-1][j-weight], table[i-1][j]) 
+            # 타겟이 된 짐을 넣을 수 있으면, 그 짐을 넣고 남는 무게의 최댓값을 더해서 위의 값과 비교한다.
+        else:
+            table[i][j] = table[i-1][j] # 타겟이된 짐을 넣을 수 없다면 위의 값을 가져온다.
+
+print(table[N][K])
